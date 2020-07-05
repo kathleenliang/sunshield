ccf6e5916a4d3fdc3cb9bdefad0bca4e69c3fc13
Kathleen Liang <liangkathleen@gmail.com>
2020-07-05 18:02:37 -0400

    fixed indentation and api call

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import classes from './Conditions.module.css'
+import React from "react";
+import classes from "./Conditions.module.css";
 
 const conditions = (props) => {
-    function getSunscreenMsg(e) {
-        if (e >= "0" && e <= "2") {
-            return "No need to wear sunscreen!";
-        } else if (e >= "3" && e <= "7") {
-            return "Wear sunscreen and a hat!";
-        } else {
-            return "Stay inside or in the shade! Wear a hat, sunglasses, and sunscreen!";
-        }
+  function getSunscreenMsg(e) {
+    if (e >= "0" && e <= "2") {
+      return "No need to wear sunscreen!";
+    } else if (e >= "3" && e <= "7") {
+      return "Wear sunscreen and a hat!";
+    } else {
+      return "Stay inside or in the shade! Wear a hat, sunglasses, and sunscreen!";
     }
+  }
 
-    return (
-       <div className = {classes.Wrapper}>
-           {props.error && <small className={classes.Small}>Please enter a valid city.</small>}
-           {props.loading && <div className={classes.Loader}>Loading...</div>}
-           {props.responseObj.location ?
-               <div>
-                   <p><strong>{props.responseObj.location.name}</strong></p>
-                   <p>The UV index is currently {props.responseObj.current.uv_index}.</p>
-                   <p>{getSunscreenMsg(props.responseObj.current.uv_index)}</p>
-               </div>
-           : null
-           }
-       </div>
-    )
-}
+  return (
+    <div className={classes.Wrapper}>
+      {props.error && (
+        <small className={classes.Small}>Please enter a valid city.</small>
+      )}
+      {props.loading && <div className={classes.Loader}>Loading...</div>}
+      {props.responseObj.data ? (
+        <div>
+          <p>
+            <strong>{props.responseObj.data[0].city_name}</strong>
+          </p>
+          <p>
+            The UV index is currently {Math.round(props.responseObj.data[0].uv)}
+            .
+          </p>
+          <p>{getSunscreenMsg(props.responseObj.data[0].uv)}</p>
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
 export default conditions;
--- src/components/UV/UV.js
+++ src/components/UV/UV.js
@@ -1,58 +1,63 @@
-import React, {useState} from 'react';
-import Conditions from '../Conditions/Conditions';
-import classes from './UV.module.css';
+import React, { useState } from "react";
+import Conditions from "../Conditions/Conditions";
+import classes from "./UV.module.css";
 
 const UV = () => {
-    let [responseObj, setResponseObj] = useState({});
-    let [city, setCity] = useState('');
-    let [error, setError] = useState(false);
-    let [loading, setLoading] = useState(false);
+  let [responseObj, setResponseObj] = useState({});
+  let [city, setCity] = useState("");
+  let [error, setError] = useState(false);
+  let [loading, setLoading] = useState(false);
 
-    function getUV(e) {
-        e.preventDefault();
+  function getUV(e) {
+    e.preventDefault();
 
-        if (city.length === 0) {
-            return setError(true);
-        }
-
-        setError(false);
-        setResponseObj({});
-        setLoading(true);
-
-        let uriEncodedCity = encodeURIComponent(city);
-
-        fetch(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${uriEncodedCity}`)
-        .then(response => response.json())
-        .then(response => {
-            if (!response.location) {
-                throw new Error()
-            }
-           setResponseObj(response);
-           setLoading(false);
-       })
-       .catch(err => {
-            setError(true);
-            setLoading(false);
-            console.log(err.message);
-        });
+    if (city.length === 0) {
+      return setError(true);
     }
-    
-    return (
-        <div>
-            <h2>Find the UV index</h2>
-            <form onSubmit={getUV}>
-                <input type="text"
-                placeholder="Enter city"
-                maxLength="50"
-                className={classes.textInput}
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
-                />
-                <button className={classes.Button} type="submit">Get UV</button>
-            </form>
-            <Conditions responseObj = {responseObj} error = {error} loading = {loading} />
-        </div>
+
+    setError(false);
+    setResponseObj({});
+    setLoading(true);
+
+    let uriEncodedCity = encodeURIComponent(city);
+
+    fetch(
+      `https://api.weatherbit.io/v2.0/current?city=${uriEncodedCity}&key=${process.env.REACT_APP_API_KEY}`
     )
-}
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.error) {
+          throw new Error();
+        }
+        setResponseObj(response);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(true);
+        setLoading(false);
+        console.log(err.message);
+      });
+  }
+
+  return (
+    <div>
+      <h2>Find the UV index</h2>
+      <form onSubmit={getUV}>
+        <input
+          type="text"
+          placeholder="Enter city"
+          maxLength="50"
+          className={classes.textInput}
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button className={classes.Button} type="submit">
+          Get UV
+        </button>
+      </form>
+      <Conditions responseObj={responseObj} error={error} loading={loading} />
+    </div>
+  );
+};
 
 export default UV;
