d3c60439b55b0a34382a8dcaa42d7b5d8878d963
Kathleen Liang <liangkathleen@gmail.com>
2020-07-04 23:04:41 -0400

    Updates

---
+++ precache-manifest.9c1e8bf9b23ec9ae35efe539615439a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9493983ebeb3a7f587c6f44ac9770c4",
+    "url": "/sunshield/index.html"
+  },
+  {
+    "revision": "540b63b3d6bc5ceb541a",
+    "url": "/sunshield/static/css/main.69def48a.chunk.css"
+  },
+  {
+    "revision": "0f0f04b2d6b215457899",
+    "url": "/sunshield/static/js/2.c9ca7d37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sunshield/static/js/2.c9ca7d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540b63b3d6bc5ceb541a",
+    "url": "/sunshield/static/js/main.a8240c5e.chunk.js"
+  },
+  {
+    "revision": "2735edf72c4a90897f21",
+    "url": "/sunshield/static/js/runtime-main.1ea5109c.js"
+  }
+]);
